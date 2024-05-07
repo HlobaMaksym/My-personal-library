dcfc4871237c09f30fdb80746ae2cbbbe4052bd4
Максим <maksgloba4@gmail.com>
2024-05-07 17:51:05 +0300

    Add project files.

---
+++ MyFirstLibrary/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstLibrary
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Autor { get; set; }
+        public string DataOfPublic { get; set; }
+
+        public Book(int id, string title, string autor, string dataOfPublic)
+        {
+            Id = id;
+            Title = title;
+            Autor = autor;
+            DataOfPublic = dataOfPublic;
+        }
+    }
+}
---
+++ MyFirstLibrary/Library.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstLibrary
+{
+    public class Library
+    {
+        public List<Book> Books;
+        public Library(List<Book> books)
+        {
+            Books = books;
+        }
+    }
+}
